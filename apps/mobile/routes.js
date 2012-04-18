3957809e54a5ed42032aac182294353e507ead87
Matti Ryhänen <mryhanen@gmail.com>
2012-04-18 12:16:59 +0200

    just pick the latest from db + show recipe icon

--- apps/mobile/routes.js
+++ apps/mobile/routes.js
@@ -2,17 +2,10 @@ var routes = function(app) {
 
 	var mongoose 	= require('mongoose');
 
-	var getDateQuery = function() {
-		var today = new Date();
-		today.setHours(0);today.setMinutes(0); today.setSeconds(0);
-		var tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1);
-
-		return {$gte: today, $lt: tomorrow};
-	}
-
 	var doRequest = function(res, model, view) {
-		model.findOne({date: getDateQuery()}, function(error, data) {
+		model.findOne().sort('date', -1).run(function(error, data) {
 			if (error == null) {
+				console.log(data);
 				res.render(__dirname + "/views/" + view, data);
 			}
 			else {
