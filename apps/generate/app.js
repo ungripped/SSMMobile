12d4474886797ac5705e57385aafec9cf5709718
Matti Ryhänen <mryhanen@gmail.com>
2012-05-12 01:33:17 +0200

    wait a bit...

--- apps/generate/app.js
+++ apps/generate/app.js
@@ -11,7 +11,15 @@ var doSSMRequest = function(url, Model, title, cb) {
 			list: data
 		});
 		console.log("Saving list: " + title + "...");
-		list.save();
+		list.save(function(err) { 
+			if (err) {
+				console.log("Could not save list: " );
+				console.log(err);
+			}
+			else {
+				console.log("List saved...");
+			}
+		});
 		cb();
 	});
 }
@@ -20,13 +28,19 @@ module.exports = {
 	articles: function(mongoose) {
 		var url = "http://xn--ssongsmat-v2a.nu/w/api.php?action=ssmlista&sasong=3&ns=0&props=kategori%2Cbild%2CNyckel&format=json";
 		doSSMRequest(url, mongoose.model('ArticleList'), 'Råvaror', function() {
-			process.exit(0);
+			setTimeout(function() {
+				console.log("Quitting...");
+				process.exit(0);
+			}, 2000);
 		});
 	},
 	recipes: function(mongoose) {
 		var url = "http://xn--ssongsmat-v2a.nu/w/api.php?action=ssmlista&sasong=3&kategori=Recept&props=kategori%2Cbild%2CNyckel&format=json";
 		doSSMRequest(url, mongoose.model('RecipeList'), 'Recept', function() {
-			process.exit(0);
+			setTimeout(function() {
+				console.log("Quitting...");
+				process.exit(0);
+			}, 2000);
 		});
 	}
 }	
